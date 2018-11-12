cfb882c81ed391d120acecbd92858db7031ba45d
jeon9825 <jeon9825@naver.com>
2018-11-12 20:35:20 +0900

    181112 sming [Button][Start Button]

--- 03) SMING/Sming/app/src/main/java/com/sming/MainActivity.java
+++ 03) SMING/Sming/app/src/main/java/com/sming/MainActivity.java
@@ -1,5 +1,6 @@
 package com.sming;
 
+import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
@@ -12,6 +13,8 @@ import android.media.Image;
 import android.media.ImageReader;
 import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
+import android.net.Uri;
+import android.os.Environment;
 import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
@@ -22,9 +25,11 @@ import android.view.Display;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.Buffer;
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
     // 종료와 시작을 정의하기 위해서 전역으로 정의해주었습니다.
@@ -76,50 +81,82 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 //타 어플(멜론)의 Activity 실행
-                ComponentName cn = new ComponentName("com.iloen.melon", "com.iloen.melon.MusicBrowserActivity");
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_LAUNCHER);
-                intent.setComponent(cn);
-                startActivity(intent);
+                ComponentName cn;
+                try {
+                    cn = new ComponentName("com.iloen.melon", "com.iloen.melon.MusicBrowserActivity");
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                    intent.setComponent(cn);
+                    startActivity(intent);
+                } catch (ActivityNotFoundException ex) { //버튼을 누를때 해당 어플이 없으면 화면이 꺼지는 오류를 try-catch문으로!
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setData(Uri.parse("market://details?id=" + "com.iloen.melon"));
+                    startActivity(intent);
+                }
             }
         });
 
         final Button genieButton = (Button) findViewById(R.id.GenieButton);
-        melonButton.setOnClickListener(new Button.OnClickListener(){
+        genieButton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
-                //타 어플(멜론)의 Activity 실행
-                ComponentName cn = new ComponentName("com.ktmusic.geniemusic", "com.ktmusic.geniemusic.DummyActivity");
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_LAUNCHER);
-                intent.setComponent(cn);
-                startActivity(intent);
+                //타 어플(지니)의 Activity 실행
+                ComponentName cn;
+                try {
+                    cn = new ComponentName("com.ktmusic.geniemusic", "com.ktmusic.geniemusic.DummyActivity");
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                    intent.setComponent(cn);
+                    startActivity(intent);
+                } catch (ActivityNotFoundException ex) { //버튼을 누를때 해당 어플이 없으면 화면이 꺼지는 오류를 try-catch문으로!
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setData(Uri.parse("market://details?id=" + "com.ktmusic.geniemusic"));
+                    startActivity(intent);
+                }
             }
         });
 
         final Button bugsButton = (Button) findViewById(R.id.BugsButton);
-        melonButton.setOnClickListener(new Button.OnClickListener(){
+        bugsButton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
-                //타 어플(멜론)의 Activity 실행
-                ComponentName cn = new ComponentName("com.neowiz.android.bugs", "com.neowiz.android.bugs.activity.HomeActivity");
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_LAUNCHER);
-                intent.setComponent(cn);
-                startActivity(intent);
+                //타 어플(벅스)의 Activity 실행
+                ComponentName cn;
+                try {
+                    cn = new ComponentName("com.neowiz.android.bugs", "com.neowiz.android.bugs.activity.HomeActivity");
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                    intent.setComponent(cn);
+                    startActivity(intent);
+                } catch (ActivityNotFoundException ex) { //버튼을 누를때 해당 어플이 없으면 화면이 꺼지는 오류를 try-catch문으로!
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setData(Uri.parse("market://details?id=" + "com.neowiz.android.bugs"));
+                    startActivity(intent);
+                }
             }
         });
 
         final Button youtubeButton = (Button) findViewById(R.id.YoutubeButton);
-        melonButton.setOnClickListener(new Button.OnClickListener(){
+        youtubeButton.setOnClickListener(new Button.OnClickListener(){
             @Override
             public void onClick(View view) {
-                //타 어플(멜론)의 Activity 실행
-                ComponentName cn = new ComponentName("com.google.android.youtube", "com.google.android.youtube.app.honeycomb.Shell$HomeActivity");
-                Intent intent = new Intent(Intent.ACTION_MAIN);
-                intent.addCategory(Intent.CATEGORY_LAUNCHER);
-                intent.setComponent(cn);
-                startActivity(intent);
+                //타 어플(유튜브)의 Activity 실행
+                ComponentName cn;
+                try {
+                    cn = new ComponentName("com.google.android.youtube", "com.google.android.youtube.app.honeycomb.Shell$HomeActivity");
+                    Intent intent = new Intent(Intent.ACTION_MAIN);
+                    intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                    intent.setComponent(cn);
+                    startActivity(intent);
+                } catch (ActivityNotFoundException ex) { //버튼을 누를때 해당 어플이 없으면 화면이 꺼지는 오류를 try-catch문으로!
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setData(Uri.parse("market://details?id=" + "com.google.android.youtube"));
+                    startActivity(intent);
+                }
             }
         });
 
@@ -137,7 +174,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_CODE) {
-            // for statistics -- init
             imagesProduced = 0;
             startTimeInMillis = System.currentTimeMillis();
 
@@ -152,12 +188,41 @@ public class MainActivity extends AppCompatActivity {
                 display.getSize(size);
                 final int width = size.x;
                 final int height = size.y;
+                final String STORE_DIRECTORY = Environment.getExternalStorageDirectory()+"/DCIM";
+                final File file = new File(STORE_DIRECTORY);
 
                 mImageReader = ImageReader.newInstance(width, height, ImageFormat.JPEG, 2);
                 mProjection.createVirtualDisplay("screen capture", width, height, density, flags, mImageReader.getSurface(), new VirtualDisplayCallback(), mHandler);
+                mImageReader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+                    @Override
+                    public void onImageAvailable(ImageReader reader) {
+                        Image image = null;
+                        FileOutputStream fos = null;
+                        Bitmap bitmap = null;
+                        try{
+                            image = mImageReader.acquireLatestImage();
+                            if (image != null){
+                                // 여기 밑에서 부터 오류가 생기는건 알겠음. 근데 왜 오류 생기는지 모를일임
+//                                Image.Plane[] planes = image.getPlanes();
+//                                ByteBuffer buffer = planes[0].getBuffer();
+//                                int pixelStride = planes[0].getPixelStride();
+//                                int rowStride = planes[0].getRowStride();
+//                                int rowPadding = rowStride - pixelStride * width;
+//                                bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888);
+//                                bitmap.copyPixelsFromBuffer(buffer);
+
+//                                //그 다음 저장하는 부분
+//                                fos = new FileOutputStream(STORE_DIRECTORY + "/myscreen_" + imagesProduced + ".jpg");
+//                                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+//                                imagesProduced++;
+                            }
+                        } catch (Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                },mHandler);
             }
         }
-
         super.onActivityResult(requestCode, resultCode, data);
     }
 
